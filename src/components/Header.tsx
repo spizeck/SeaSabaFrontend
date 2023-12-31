d873e0474f8baf20cdaa1ee6c7fc48285626ab4e
Chad <chadnuttall1@gmail.com>
2023-12-31 11:28:33 -0400

    Added dialog and dialog UI, updated package dependencies
    Added the package dependencies "@radix-ui/react-dialog" and "react-icons", and implemented new dialog UI components in "sheet.tsx". Also made minor housekeeping changes in the MainLayout.tsx and Header.tsx structure for better readability. Finally, updated the package-lock.json to reflect the newly added dependencies.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,44 +1,67 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Image from 'next/image'
 import Link from "next/link"
-import {cn} from "@/lib/utils"
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
+import { FaBars, FaTimes } from'react-icons/fa'
+
 
 const Header: React.FC = () => {
 
+  const [nav, setNav] = useState(false)
+
+  const links = [
+    {
+      name: 'Home',
+      link: '/'
+    },
+    {
+      name: 'About',
+      link: '/about'
+    },
+    {
+      name: 'Group Contracts',
+      link: '/group-contracts'
+    },
+    {
+      name: 'Preferences',
+      link: '/preferences'
+    }
+  ]
+
   return (
-    <header className='p-4 border-b-2'>
-      <div className='flex justify-between items-center'>
+    <header className="bg-blue-950 text-white p-4 border-b-2">
+      <div className="flex justify-between items-center">
         <div className="nav-logo">
-          <Image
-            src="/ColorLogo.png"
-            alt="logo"
-            width={250}
-            height={150}
-          />
+          <Link href="/">
+              <Image src="/WhiteLogo.png" alt="logo" width={250} height={150} />
+          </Link>
         </div>
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink>Link </NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
 
+        <div className="hidden md:flex">
+          {links.map((link) => (
+            <Link href={link.link} key={link.name}>
+              <div className="mx-4 hover:text-gray-300">{link.name}</div>
+            </Link>
+          ))}
+        </div>
+
+        <div className="md:hidden">
+          {nav ? (
+            <FaTimes onClick={() => setNav(false)} />
+          ) : (
+            <FaBars onClick={() => setNav(true)} />
+          )}
+        </div>
       </div>
-    </header>
-  )
-}
 
-export default Header
+      <nav className={nav ? 'flex' : 'hidden'} onClick={() => setNav(false)}>
+        {links.map((link) => (
+          <Link href={link.link} key={link.name}>
+            <div className="block p-4 text-sm hover:bg-gray-700">{link.name}</div>
+          </Link>
+        ))}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,14 +1,14 @@
-import React, { ReactNode } from 'react';
-import Header from '@/components/Header'; // Ensure the path is correct based on your project structure
+import React, {ReactNode} from 'react';
+import Header from '@/components/Header';
 
 type MainLayoutProps = {
   children: ReactNode;
 };
 
-const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+const MainLayout: React.FC<MainLayoutProps> = ({children}) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
+      <Header/>
       <main className="flex-grow mx-auto px-2 py-2">
         {children}
       </main>
