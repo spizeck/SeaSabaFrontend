5b8117730153fb10c07e5353934523337fda73a6
Chad <chadnuttall1@gmail.com>
2024-01-01 13:47:36 -0400

    Add Admin, Logout and Register pages; enhance Header
    Added new Admin, Logout and Register pages to the project and moved Login page to Register page. Made substantial enhancements to Header component, introducing buttons and dialogs for authentication actions. Also, made changes for better structure readability.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,18 @@
 import React, {useState} from 'react'
 import Image from 'next/image'
 import Link from "next/link"
-import { FaBars, FaTimes } from'react-icons/fa'
-
+import {FaBars} from 'react-icons/fa'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import {Button} from "@/components/ui/button"
 
 const Header: React.FC = () => {
-
-  const [nav, setNav] = useState(false)
 
   const links = [
     {
@@ -21,45 +27,101 @@ const Header: React.FC = () => {
       name: 'Group Contracts',
       link: '/group-contracts'
     },
-    {
-      name: 'Preferences',
-      link: '/preferences'
-    }
   ]
+
+  const renderAuthButtons = () => {
+    const isLoggedIn = false
+    // TODO: get this from session
+    if (isLoggedIn) {
+      return (
+        <>
+            <Sheet>
+              <SheetTrigger>
+                <Button variant="ghost">Preferences</Button>
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>User Preferences</SheetTitle>
+                  <SheetDescription>Manage your user preferences</SheetDescription>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+          <Button variant="ghost" onClick={() => {
+          }}>Logout</Button>
+        </>
+      )
+    }
+    return (
+      <>
+            <Sheet>
+              <SheetTrigger>
+                <Button variant="ghost">Register</Button>
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>User Registration</SheetTitle>
+                  <SheetDescription>Please fill in the following fields to register a new user</SheetDescription>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+            <Sheet>
+              <SheetTrigger>
+                <Button variant="ghost">Login</Button>
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>Please Log In</SheetTitle>
+                  <SheetDescription>Please enter your username and password to login</SheetDescription>
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+      </>
+    )
+  }
 
   return (
     <header className="bg-blue-950 text-white p-4 border-b-2">
       <div className="flex justify-between items-center">
-        <div className="nav-logo">
-          <Link href="/">
-              <Image src="/WhiteLogo.png" alt="logo" width={250} height={150} />
-          </Link>
+        <div className="flex items-center">
+          <div className="nav-logo">
+            <Link href="/">
+              <Image src="/WhiteLogo.png" alt="logo" width={250} height={150}/>
+            </Link>
+          </div>
+          <div className="hidden md:flex px-4">
+            {links.map((link) => (
+              <Link href={link.link} key={link.name}>
+                <Button variant="ghost">{link.name}</Button>
+              </Link>
+            ))}
+          </div>
         </div>
 
         <div className="hidden md:flex">
-          {links.map((link) => (
-            <Link href={link.link} key={link.name}>
-              <div className="mx-4 hover:text-gray-300">{link.name}</div>
-            </Link>
-          ))}
+          {renderAuthButtons()}
         </div>
 
-        <div className="md:hidden">
-          {nav ? (
-            <FaTimes onClick={() => setNav(false)} />
-          ) : (
-            <FaBars onClick={() => setNav(true)} />
-          )}
+
+        <div className="md:hidden flex items-center">
+          <div className="p-4 flex-grow">
+            {renderAuthButtons()}
+          </div>
+          <Sheet>
+            <SheetTrigger className="p-4" aria-label="Menu"><FaBars/></SheetTrigger>
+            <SheetContent side='top'>
+              <SheetHeader>
+                <SheetTitle>Navigation</SheetTitle>
+              </SheetHeader>
+              {links.map((link) => (
+                <Link href={link.link} key={link.name}>
+                  <Button variant="link" className="block mb-2">{link.name}</Button>
+                </Link>
+              ))}
+
+            </SheetContent>
+          </Sheet>
         </div>
       </div>
-
-      <nav className={nav ? 'flex' : 'hidden'} onClick={() => setNav(false)}>
-        {links.map((link) => (
-          <Link href={link.link} key={link.name}>
-            <div className="block p-4 text-sm hover:bg-gray-700">{link.name}</div>
-          </Link>
-        ))}
-      </nav>
     </header>
   );
 };
