7426fa9f30e4dbd7a91358ffa83586a49e65e1ce
Chad Nuttall <chadnuttall1@gmail.com>
2024-02-27 19:42:09 -0400

    Add .idea configuration files and refactor form components
    Added .idea configuration files to enhance IDE support . Also, refactored 'AddHotelForm' and 'GroupContract' components to simplify the form handling. This includes removing some form validation rules in 'AddHotelForm' and handling 'onSubmit' event within this component instead of passing it as a prop. Additionally, 'GroupContract' component has been simplified to only render the 'AddHotelForm' for clearer structure.

--- src/components/forms/AddHotelForm.tsx
+++ src/components/forms/AddHotelForm.tsx
@@ -2,61 +2,47 @@ import * as React from 'react';
 import {useForm} from 'react-hook-form';
 import {TextField, Grid, Button, Typography} from '@mui/material';
 
-export function AddHotelForm(onSubmit) {
+export function AddHotelForm() {
   const {register, handleSubmit, formState: {errors}} = useForm();
-
+  const onSubmit = (data) => {
+    console.log(data);
+  }
   return (
-    <form noValidate onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Typography variant="h6" gutterBottom>
         Add New Hotel
       </Typography>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={6}>
           <TextField
-            {...register('name', {
+            {...register('hotel-name', {
               required: "Hotel name is required",
-              maxLength: {
-                value: 50,
-                message: "The hotel name should have at most 50 characters",
-              }
             })}
             error={Boolean(errors.name)}
             helperText={errors.name?.message as string}
             required
             fullWidth
             label="Hotel Name"
-            name="name"
-            autoComplete="name"
+            name="hotel-name"
             autoFocus
           />
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
-            {...register('location', {
-              required: "Location is required",
-              maxLength: {
-                value: 100,
-                message: "The location should have at most 100 characters",
-              }
-            })}
+            {...register('location')}
             error={Boolean(errors.location)}
             helperText={errors.location?.message as string}
-            required
             fullWidth
             label="Location"
             name="location"
-            autoComplete="location"
           />
         </Grid>
 
         <Grid item xs={12} sm={6}>
           <TextField
-            {...register('contact_info', {
-              required: "Contact info is required",
-            })}
+            {...register('contact_info')}
             error={Boolean(errors.contact_info)}
             helperText={errors.contact_info?.message as string}
-            required
             fullWidth
             label="Contact Info"
             name="contact_info"
@@ -70,7 +56,6 @@ export function AddHotelForm(onSubmit) {
             fullWidth
             label="Amenities"
             name="amenities"
-            autoComplete="amenities"
             multiline
             rows={2}
           />
@@ -81,7 +66,6 @@ export function AddHotelForm(onSubmit) {
             fullWidth
             label="Policies"
             name="policies"
-            autoComplete="policies"
             multiline
             rows={4}
           />
@@ -92,7 +76,6 @@ export function AddHotelForm(onSubmit) {
             fullWidth
             label="Description"
             name="description"
-            autoComplete="description"
             multiline
             rows={4}
           />
--- src/pages/group-contracts.tsx
+++ src/pages/group-contracts.tsx
@@ -1,24 +1,15 @@
 import * as React from 'react'
-import {useForm} from 'react-hook-form'
-import {TextField, Button, Checkbox, FormControlLabel} from '@mui/material'
-import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import MainLayout from "@/layouts/MainLayout";
 import {AddHotelForm} from "@/components/forms/AddHotelForm";
 
-export default function GroupContract() {
-  const {register, handleSubmit, formState: {errors}} = useForm();
 
-  const onSubmit = (data: any) => {
-    console.log(data);
-  };
+export default function GroupContract() {
 
   return (
     <MainLayout>
       <Container>
-<AddHotelForm onSubmit={handleSubmit(onSubmit)} errors={errors} register={register} />
+        <AddHotelForm />
       </Container>
     </MainLayout>
   )
